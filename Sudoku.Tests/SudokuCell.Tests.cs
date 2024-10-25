b412fd67d415f04e85353816915bf3499692cf5a
Sam Graber <authorsamgraber@gmail.com>
2024-10-25 14:06:49 -0400

    Added unit test project
    We want to be able to run unit tests to verify our implementation details. This requires restructuring the repository so the main project has a nested folder under the solution, and we have a second project for tests
    
    Implemented an initial test to demonstrate some of the cell logic

---
+++ Sudoku.Tests/SudokuCell.Tests.cs
@@ -0,0 +1,23 @@
+using Bunit;
+using Sudoku.Components.Pages;
+
+public class SudokuTests
+{
+    [Fact]
+    public void Should_clear_the_cell_on_a_delete_keypress()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var cut = ctx.RenderComponent<SudokuCell>(parameters => parameters
+            .Add(p => p.Cell, new Cell(0, 0)));
+        var cellElement = cut.Find(".cell");
+
+        // Act
+        cut.Find(".cell").KeyDown("Delete");
+
+        // Assert
+        var cellText = cellElement.TextContent;
+        cellText.MarkupMatches("0");
+        cut.Instance.Cell.Value.Equals(0);
+    }
+}
