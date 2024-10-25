d7f0e07b4889107c3f89f66f43352eb99597e450
Sam Graber <authorsamgraber@gmail.com>
2024-10-25 14:59:45 -0400

    Renaming for clarity
    I took these initial names from a blazor unit test example, but I would like to use clearer names where possible

--- Sudoku.Tests/SudokuCell.Tests.cs
+++ Sudoku.Tests/SudokuCell.Tests.cs
@@ -1,7 +1,7 @@
 using Bunit;
 using Sudoku.Components.Pages;
 
-public class SudokuTests
+public class SudokuCellTests
 {
     [Theory]
     [InlineData("1", 1)]
@@ -16,18 +16,18 @@ public class SudokuTests
     public void Should_set_the_cell_value_and_invoke_a_change_event_on_a_number_key(string numberEntry, int numberValue)
     {
         // Arrange
-        using var ctx = new TestContext();
+        using var context = new TestContext();
         int? valueOnParent = null;
-        var cut = ctx.RenderComponent<SudokuCell>(parameters => parameters
+        var cellComponent = context.RenderComponent<SudokuCell>(parameters => parameters
             .Add(p => p.Cell, new Cell(0, 0))
             .Add(p => p.SetValue, x => valueOnParent = x));
-        var cellElement = cut.Find(".cell");
+        var cellElement = cellComponent.Find(".cell");
 
         // Act
-        cut.Find(".cell").KeyDown(numberEntry);
+        cellComponent.Find(".cell").KeyDown(numberEntry);
 
         // Assert
-        Assert.Equal(numberValue, cut.Instance.Cell.Value);
+        Assert.Equal(numberValue, cellComponent.Instance.Cell.Value);
         Assert.Equal(numberValue, valueOnParent);
     }
 
@@ -37,18 +37,18 @@ public class SudokuTests
     public void Should_clear_the_cell_on_a_delete_or_backspace_keypress(string keypress)
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<SudokuCell>(parameters => parameters
+        using var context = new TestContext();
+        var cellComponent = context.RenderComponent<SudokuCell>(parameters => parameters
             .Add(p => p.Cell, new Cell(0, 0)));
-        var cellElement = cut.Find(".cell");
+        var cellElement = cellComponent.Find(".cell");
 
         // Act
-        cut.Find(".cell").KeyDown(keypress);
+        cellComponent.Find(".cell").KeyDown(keypress);
 
         // Assert
         var cellText = cellElement.TextContent;
         cellText.MarkupMatches("");
-        Assert.Null(cut.Instance.Cell.Value);
+        Assert.Null(cellComponent.Instance.Cell.Value);
     }
 
     [Theory]
@@ -59,24 +59,24 @@ public class SudokuTests
     public void Should_send_a_change_focus_event_to_the_parent_on_an_arrow_key(string arrowKey)
     {
         // Arrange
-        using var ctx = new TestContext();
+        using var context = new TestContext();
         var cellOnParent = new Cell(0, 0);
         var changeEventOnParent = "";
-        var cut = ctx.RenderComponent<SudokuCell>(parameters => parameters
+        var cellComponent = context.RenderComponent<SudokuCell>(parameters => parameters
             .Add(p => p.Cell, new Cell(0, 0))
             .Add(p => p.ChangeFocus, focusEvent => {
                 cellOnParent = focusEvent.Cell;
                 changeEventOnParent = focusEvent.ChangeEvent;
             }));
-        var cellElement = cut.Find(".cell");
+        var cellElement = cellComponent.Find(".cell");
 
         // Act
-        cut.Find(".cell").KeyDown(arrowKey);
+        cellComponent.Find(".cell").KeyDown(arrowKey);
 
         // Assert
         var cellText = cellElement.TextContent;
         cellText.MarkupMatches("");
-        Assert.Equal(cut.Instance.Cell, cellOnParent);
+        Assert.Equal(cellComponent.Instance.Cell, cellOnParent);
         Assert.Equal(arrowKey, changeEventOnParent);
     }
 }
