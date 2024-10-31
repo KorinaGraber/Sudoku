0eb8c9f7fe8de2409049ce17ffbc305a373c4ae8
Sam Graber <authorsamgraber@gmail.com>
2024-10-31 17:45:32 -0400

    Allow for updating a cell via the cell buttons
    Now that we can rely on the focused cell being set, we can use the button actions to update the value accordingly. This requires a state change notification since the child doesn't know when a property of cell is being updated

--- SudokuTests/SudokuGrid.Tests.cs
+++ SudokuTests/SudokuGrid.Tests.cs
@@ -242,4 +242,78 @@ public class SudokuGridTests
         // Assert
         Assert.Null(gridComponent.Instance.FocusedCell);
     }
+
+    [Theory]
+    [InlineData("1")]
+    [InlineData("2")]
+    [InlineData("3")]
+    [InlineData("4")]
+    [InlineData("5")]
+    [InlineData("6")]
+    [InlineData("7")]
+    [InlineData("8")]
+    [InlineData("9")]
+    public void Should_update_the_value_of_the_focused_cell_when_a_number_is_entered(string numberAsString)
+    {
+        // Arrange
+        using var context = new TestContext();
+        var random = new Random();
+        var gridComponent = context.RenderComponent<SudokuGrid>();
+        var currentColumn = random.Next(0, 8);
+        var currentRow = random.Next(0, 8);
+        var expectedValue = int.Parse(numberAsString);
+        var focusedCell = gridComponent.Instance.Grid[currentColumn, currentRow];
+        gridComponent.Instance.FocusedCell = focusedCell;
+
+        // Act
+        gridComponent.Instance.NumberEntered(numberAsString);
+
+        // Assert
+        Assert.Equal(expectedValue, gridComponent.Instance.FocusedCell.Value);
+    }
+
+    [Theory]
+    [InlineData("0")]
+    [InlineData("10")]
+    public void Should_ignore_an_update_if_the_value_is_out_of_range(string numberAsString)
+    {
+        // Arrange
+        using var context = new TestContext();
+        var random = new Random();
+        var gridComponent = context.RenderComponent<SudokuGrid>();
+        var currentColumn = random.Next(0, 8);
+        var currentRow = random.Next(0, 8);
+        var expectedValue = int.Parse(numberAsString);
+        var oldValue = random.Next();
+        var focusedCell = gridComponent.Instance.Grid[currentColumn, currentRow];
+        focusedCell.Value = oldValue;
+        gridComponent.Instance.FocusedCell = focusedCell;
+
+        // Act
+        gridComponent.Instance.NumberEntered(numberAsString);
+
+        // Assert
+        Assert.Equal(oldValue, gridComponent.Instance.FocusedCell.Value);
+    }
+
+    [Fact]
+    public void Should_delete_the_value_of_the_focused_cell()
+    {
+        // Arrange
+        using var context = new TestContext();
+        var random = new Random();
+        var gridComponent = context.RenderComponent<SudokuGrid>();
+        var currentColumn = random.Next(0, 8);
+        var currentRow = random.Next(0, 8);
+        var oldValue = random.Next();
+        var focusedCell = gridComponent.Instance.Grid[currentColumn, currentRow];
+        focusedCell.Value = oldValue;
+        gridComponent.Instance.FocusedCell = focusedCell;
+
+        // Act
+        gridComponent.Instance.Delete();
+
+        // Assert
+        Assert.Null(gridComponent.Instance.FocusedCell.Value);
+    }
 }
